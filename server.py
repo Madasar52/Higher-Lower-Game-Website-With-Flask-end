da5fe5f10964e56dfcb3ac28a6ef06138f5a3994
Madasar <madasar54321@gmail.com>
2024-04-04 23:46:36 -0400

    Commit

---
+++ server.py
@@ -0,0 +1,53 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+random_number = random.randint(0,9)
+print(random_number)
+
+@app.route('/')
+def main_page():
+    return ('<h1> Guess a number between 0 and 9 </h1>'
+            '<img src = "https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif"/>')
+
+@app.route('/<number>')
+def number_checker(number):
+    if int(number)>random_number:
+        return ('<h1> Too high, try again! </h1>'
+                '<img src = "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"/>')
+
+    if int(number) < random_number:
+        return ('<h1> Too low, try again! </h1>'
+                '<img src = "https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif"/>')
+
+    if int(number) == random_number:
+        return ('<h1> You found me! </h1>'
+                '<img src = "https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif"/>')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
